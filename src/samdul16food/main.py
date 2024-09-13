3c4802a61517bff6e47a24d1da9be80938637c80
j25ng <jsan2861@gmail.com>
2024-09-13 14:00:28 +0900

    add option

--- src/samdul16food/main.py
+++ src/samdul16food/main.py
@@ -1,9 +1,22 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import time
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8899",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
